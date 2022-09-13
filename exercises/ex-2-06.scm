15e34604cd12e77b5bbda5ef64be1a9d2b12ce47
Patrick Bucher <patrick.bucher@mailbox.org>
2022-09-13 07:21:26 +0200

    exercise 2.6

--- exercises/ex-2-06.scm
+++ exercises/ex-2-06.scm
@@ -10,5 +10,59 @@
 ;; (add-one zero)
 ;; (lambda (f) (lambda (x) (f ((n f) x))))
 ;; (lambda (f) (lambda (x) (f ((zero f) x))))
-;; (lambda (f) (lambda (x) (f ((lambda (f) (lambda (x) x)) f) x)))
-;; (lambda (f) (lambda 
+
+;; evaluate from the inside out:
+;; (zero f)
+;; zero: rename f -> g, x -> y for clarity
+;; (define (zero (lambda (g) (lambda (y) y))))
+;; ((lambda (g) (lambda (y) y)) f)
+;; (lambda (y) y)
+
+;; put back in (lambda (y) y) for (zero f)
+;; (lambda (f) (lambda (x) (f ((lambda (y) y) x))))
+
+;; evaluate from the inside out:
+;; ((lambda (y) y) x)
+;; x
+
+;; put back in
+;; (lambda (f) (lambda (x) (f x)))
+
+;; definitions:
+;; zero
+;; (lambda (f) (lambda (x) x))
+;; (add-1 zero): one
+;; (lambda (f) (lambda (x) (f x)))
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+;; (add-1 (add-1 zero))
+;; (add-1 one)
+;; (lambda (f) (lambda (x) (f ((n f) x)))
+;; (lambda (f) (lambda (x) (f ((one f) x))))
+
+;; evaluate from the inside out:
+;; (one f)
+;; one: rename f -> g, x -> y for clarity
+;; (define one (lambda (g) (lambda (y) (g y))))
+;; ((lambda (g) (lambda (y) (g y))) f)
+;; (lambda (y) (f y))
+
+;; put back in (lambda (y) (f y)) for (one f)
+;; (lambda (f) (lambda (x) (f ((lambda (y) (f y)) x))))
+
+;; evaluate from the inside out:
+;; (f ((lambda (y) (f y)) x))
+;; (f (f x))
+
+;; put back in
+;; (lambda (f) (lambda (x) (f (f x)))
+
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+;; one: (f x)
+;; two: (f (f x))
+;; three: (f (f (f x)))
+;; one + two: compose a and b
+(define (add a b)
+  (lambda (x) (a (b x))))
