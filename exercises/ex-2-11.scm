bbe18b126563d2a0116e8565b4b88e45931b60ac
Patrick Bucher <patrick.bucher@mailbox.org>
2022-09-17 09:18:41 +0200

    exercise 2.11: cases 1-3 (of 9)

--- exercises/ex-2-11.scm
+++ exercises/ex-2-11.scm
@@ -1,14 +1,18 @@
-(define (mul-interval x y)
-  (let ((p1 (* (lower-bound x)
-	       (lower-bound y)))
-	(p2 (* (lower-bound x)
-	       (upper-bound y)))
-	(p3 (* (upper-bound x)
-	       (lower-bound y)))
-	(p4 (* (upper-bound x)
-	       (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4)
-		   (max p1 p2 p3 p4))))
+;; test routine to find min/max product by brute force
+(define (min-max-prod x y)
+  (let ((x-l (lower-bound x))
+	(x-u (upper-bound x))
+	(y-l (lower-bound y))
+	(y-u (upper-bound y)))
+    (make-interval
+     (min (* x-l y-l)
+	  (* x-l y-u)
+	  (* x-u y-l)
+	  (* x-u y-u))
+     (max (* x-l y-l)
+	  (* x-l y-u)
+	  (* x-u y-l)
+	  (* x-u y-u)))))
 
 ;; two numbers
 ;; two endpoints per numbers, i.e. four endpoints
@@ -28,8 +32,35 @@
 ;; 7. a both negative; b both positive
 ;; 8. a both negative; b lower negative, upper positive
 ;; 9. a both negative; b both negative
-;;
-;; TODO: reason on what bound to take for extreme values
+
+(define (mul-interval x y)
+  (let ((x-l (lower-bound x))
+	(x-u (upper-bound x))
+	(y-l (lower-bound y))
+	(y-u (upper-bound y)))
+    (cond ((and (> x-l 0) (> x-u 0) (> y-l 0) (> y-u 0))
+	   (make-interval (* x-l y-l) (* x-u y-u)))
+	  ((and (> x-l 0) (> x-u 0) (< y-l 0) (> y-u 0))
+	   (make-interval (* x-u y-l) (* x-u y-u)))
+	  ((and (> x-l 0) (> x-u 0) (< y-l 0) (< y-u 0))
+	   (make-interval (* x-u y-l) (* x-l y-u)))
+	  (else #f))))
+
+;; case 1:
+;; (mul-interval (make-interval 0.9 1.1) (make-interval 1.8 2.2))
+;; (1.62 . 2.4200000000000004)
+;; (min-max-prod (make-interval 0.9 1.1) (make-interval 1.8 2.2))
+;; (1.62 . 2.4200000000000004)
+;; case 2:
+;; (mul-interval (make-interval 0.9 1.1) (make-interval -0.5 0.5))
+;; (-.55 . .55)
+;; (min-max-prod (make-interval 0.9 1.1) (make-interval -0.5 0.5))
+;; (-.55 . .55)
+;; case 3:
+;; (mul-interval (make-interval 1.9 2.1) (make-interval -1.5 -0.5))
+;; (-3.1500000000000004 . -.95)
+;; (min-max-prod (make-interval 1.9 2.1) (make-interval -1.5 -0.5))
+;; (-3.1500000000000004 . -.95)
 
 (define (make-interval a b)
   (cons a b))
