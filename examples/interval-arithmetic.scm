cf435da3337d900d2509a7f1e6e4a817329e39c6
Patrick Bucher <patrick.bucher@mailbox.org>
2022-09-22 08:38:03 +0200

    interval arithmetics: parallel resistors

--- examples/interval-arithmetic.scm
+++ examples/interval-arithmetic.scm
@@ -1,8 +1,34 @@
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+		(add-interval r1 r2)))
+
+;; (par1 (make-interval 90 110) (make-interval 75 85))
+;; (34.61538461538461 . 56.666666666666664)
+;; (/ (* 100. 80.) (+ 100. 80.))
+;; 44.4...
+
+(define (par2 r1 r2)
+  (let ((one (make-interval 1.0 1.0)))
+    (div-interval one
+		  (add-interval (div-interval one r1)
+				(div-interval one r2)))))
+
+;; (par2 (make-interval 90 110) (make-interval 75 85))
+;; (40.90909090909091 . 47.94871794871795)
+;; (/ 1. (+ (/ 1. 100) (/ 1. 80)))
+;; 44.4...
+
 (define (add-interval x y)
   (make-interval (+ (lower-bound x)
 		    (lower-bound y))
 		 (+ (upper-bound x)
 		    (upper-bound y))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x)
+		    (upper-bound y))
+		 (- (upper-bound x)
+		    (lower-bound y))))
 
 (define (mul-interval x y)
   (let ((p1 (* (lower-bound x)
