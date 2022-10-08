6b7a1c54bced68351e49cb49f8a5bc2ec5330f3e
Patrick Bucher <patrick.bucher@mailbox.org>
2022-10-08 10:20:42 +0200

    finished exercise 2.33

--- exercises/ex-2-33.scm
+++ exercises/ex-2-33.scm
@@ -1,13 +1,32 @@
 (define (accumulate combine initial sequence)
   (if (null? sequence)
       initial
-      (accumulate combine (combine initial (car sequence)) (cdr sequence))))
+      (combine (car sequence)
+	       (accumulate combine initial (cdr sequence)))))
 
 ;; (accumulate (lambda (x y) (+ x y)) 0 (list 1 2 3 4 5))
 ;; 15
 
 (define nil '())
 
+(define (twice x) (* x 2))
+
 (define (map p sequence)
-  (accumulate (lambda (x y) (list (p x)))
+  (accumulate (lambda (x y) (cons (p x) y))
 	      nil sequence))
+
+;; (map twice (list 1 2 3))
+;; (2 4 6)
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+;; (append (list 1 2 3) (list 4 5 6))
+;; (1 2 3 4 5 6)
+
+(define (length sequence)
+  (accumulate (lambda (elem acc) (+ acc 1))
+	      0 sequence))
+
+;; (length (list 1 2 3 4 5))
+;; 5
