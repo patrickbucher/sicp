4a03aa3823e69175227c7acd7323799186e6613c
Patrick Bucher <patrick.bucher@mailbox.org>
2022-10-19 19:26:44 +0200

    exercise 2.42: stub implementation

---
+++ exercises/ex-2-42.scm
@@ -0,0 +1,40 @@
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+	(list empty-board)
+	(filter
+	 (lambda (positions)
+	   (safe? k positions))
+	 (flatmap
+	  (lambda (rest-of-queens)
+	    (map (lambda (new-row)
+		   (adjoin-position
+		    new-row
+		    k
+		    rest-of-queens))
+		 (enumerate-interval
+		  1
+		  board-size)))
+	  (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+;; TODO: implement procedures
+;; empty-board
+;; safe
+;; adjoin-position
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (accumulate op init seq)
+  (if (null? seq)
+      init
+      (op (car seq)
+	  (accumulate op init (cdr seq)))))
+
+(define (enumerate-interval a b)
+  (define (next acc i)
+    (if (> i b)
+	acc
+	(next (append acc (list i)) (+ i 1))))
+  (next '() a))
