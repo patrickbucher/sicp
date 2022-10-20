e0abcbdb52605d009aaffda6620736396d346b80
Patrick Bucher <patrick.bucher@mailbox.org>
2022-10-20 20:27:35 +0200

    re-flowed lines

--- exercises/ex-2-42.scm
+++ exercises/ex-2-42.scm
@@ -8,13 +8,8 @@
 	 (flatmap
 	  (lambda (rest-of-queens)
 	    (map (lambda (new-row)
-		   (adjoin-position
-		    new-row
-		    k
-		    rest-of-queens))
-		 (enumerate-interval
-		  1
-		  board-size)))
+		   (adjoin-position new-row k rest-of-queens))
+		 (enumerate-interval 1 board-size)))
 	  (queen-cols (- k 1))))))
   (queen-cols board-size))
 
