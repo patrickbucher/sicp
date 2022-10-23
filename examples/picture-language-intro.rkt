ea0bcf07fb4313e16bba9fd19cb6e836210a1396
Patrick Bucher <patrick.bucher@mailbox.org>
2022-10-23 11:42:18 +0200

    picture language examples and exercise 2.44

---
+++ examples/picture-language-intro.rkt
@@ -0,0 +1,51 @@
+#lang sicp
+(#%require sicp-pict)
+
+(define wave (load-painter "wave-man.jpg"))
+(define rogers (load-painter "william-barton-rogers.jpg"))
+
+(paint wave)
+(paint rogers)
+
+(paint (beside wave rogers))
+(paint (below wave rogers))
+
+(define wave2 (beside wave (flip-vert wave)))
+(define wave4 (below wave2 wave2))
+(paint wave2)
+(paint wave4)
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define wave4b (flipped-pairs wave))
+(paint wave4b)
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter
+                (below smaller smaller)))))
+(paint (right-split rogers 4))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter
+               (beside smaller smaller)))))
+
+(paint (corner-split rogers 3))
---
+++ exercises/ex-2-44.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+(#%require sicp-pict)
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter
+                (below smaller smaller)))))
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter
+               (beside smaller smaller)))))
+
+(define rogers (load-painter "william-barton-rogers.jpg"))
+
+(paint (corner-split rogers 3))
