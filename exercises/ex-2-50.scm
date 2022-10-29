182e65f1c8e727dcde7e19dd2c4ce01f4b7057a9
Patrick Bucher <patrick.bucher@mailbox.org>
2022-10-29 08:38:17 +0200

    exercise 2.50

---
+++ exercises/ex-2-50.scm
@@ -0,0 +1,23 @@
+#lang sicp
+
+(#%require sicp-pict)
+
+(define rogers (load-painter "william-barton-rogers.jpg"))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (flip-counterclockwise-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (flip-counterclockwise-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
