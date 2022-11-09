3c0f7a3cd9709b0a612863ce7de5947c8f9b5038
Patrick Bucher <patrick.bucher@mailbox.org>
2022-11-09 07:25:54 +0100

    exercise 2.61

---
+++ exercises/ex-2-61.scm
@@ -0,0 +1,18 @@
+(define (adjoin-set x set)
+  (define (prepend left right)
+    (cond ((null? right) (append left (list x)))
+	  ((< x (car right)) (append left (list x) right))
+	  (else (prepend (append left (list (car right))) (cdr right)))))
+  (prepend '() set))
+
+(define s '(1 3 5 7 9))
+
+;; (adjoin-set 0 s)
+;; (0 1 3 5 7 9)
+;; (adjoin-set 2 s)
+;; (1 2 3 5 7 9)
+;; (adjoin-set 4 s)
+;; (1 3 4 5 7 9)
+;; (adjoin-set 10 s)
+;; (1 3 5 7 9 10)
+
