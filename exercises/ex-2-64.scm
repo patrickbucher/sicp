3e7f3b11cfe8de37d1b95c409bb41a18d47d5043
Patrick Bucher <patrick.bucher@mailbox.org>
2022-11-18 06:54:59 +0100

    exercise 2.64

--- exercises/ex-2-64.scm
+++ exercises/ex-2-64.scm
@@ -34,3 +34,18 @@
 				 right-tree)
 		      remaining-elts))))))))
 
+;; 1) The procedure `partial-tree` expects an ordered list of
+;; elements, and the length of that list. The elements are split up
+;; into two lists (left and right) of roughly equal size (+/- 1
+;; element). Each of those lists is then processed using a recursive
+;; call to `partial-tree`. The element between those two lists is
+;; turned into a tree node, to which the new left and right trees (the
+;; results of the recursive calls) are attached.  The procedure
+;; returns a cons cell, where the first element (car) represents the
+;; tree structure that was built up so far, and the second element
+;; (cdr) represents the elements that are not yet in the tree
+;; structure, and, thus, require further processing.
+
+;; 2) For every element, `partial-tree` is called twice, so the growth
+;; is 2*n. With the constant factor 2 ignored, the complexity becomes
+;; O(n), given that the input elements are sorted.
