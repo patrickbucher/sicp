9a673adf77b81d8b1a42697fde8ae07b967a5429
Patrick Bucher <patrick.bucher@mailbox.org>
2022-11-20 11:33:27 +0100

    [WIP]: exercise 2.65

--- exercises/ex-2-65.scm
+++ exercises/ex-2-65.scm
@@ -40,3 +40,45 @@
 				 left-tree
 				 right-tree)
 		      remaining-elts))))))))
+
+(define (extend s v)
+  (define (prepend left right)
+    (if (null? right)
+	(append left (list v))
+	(let ((r (car right)))
+	  (cond ((= v r) (append left right))
+		((< v r) (append left (list v) right))
+		((> v r) (prepend (append left (list r))
+				  (cdr right)))))))
+  (prepend '() s))
+
+(define (list-contains? l v)
+  (cond ((null? l) #f)
+	((= v (car l)) #t)
+	(else (list-contains? (cdr l) v))))
+
+(define (list-union-set s1 s2)
+  (define (union remainder acc)
+    (if (null? remainder)
+	acc
+	(if (list-contains? s1 (car remainder))
+	    (union (cdr remainder) (extend acc (car remainder)))
+	    (union (cdr remainder) acc))))
+  (union s2 '()))
+
+(define (tree-contains? t v)
+  (cond ((null? t) #f)
+	((= (entry t) v) #t)
+	((or (tree-contains? (left-branch t) v)
+	     (tree-contains? (right-branch t) v)))
+	(else #f)))
+
+;; TODO: descend the first tree t1, check for each element whether or
+;; not it is contained in t2. Extend the set if so, leave the set as it
+;; is otherwise. Problem: How to accumulate two result sets into one?
+
+(define t (list->tree '(1 3 5 7 9 11)))
+(define u (list->tree '(1 2 3 4 7 8 9)))
+
+;; TODO union-set
+;; TODO intersection-set
