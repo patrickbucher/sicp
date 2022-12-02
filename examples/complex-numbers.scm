ccdd9dcace2d7d80bba572029e59fae4aedc2b4d
Patrick Bucher <patrick.bucher@mailbox.org>
2022-12-02 07:45:01 +0100

    reworked complex number example with tagged data

--- examples/complex-numbers.scm
+++ examples/complex-numbers.scm
@@ -1,54 +1,119 @@
-;; Ben Bitdiddle: rectangular form
-(define (real-part z) (car z))
-(define (imag-part z) (cdr z))
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
 
-(define (magnitude z)
-  (sqrt (+ (square (real-part z))
-	   (square (imag-part z)))))
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
 
-(define (angle z)
-  (atan (imag-part z) (real-part z)))
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
 
-(define (make-from-real-imag real imag)
-  (cons real imag))
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
 
-(define (make-from-mag-ang r a)
-  (cons (* r (cos a)) (* r (sin a))))
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
 
-;; Alyssa P. Hacker: polar form
+
+;; Rectangular Representation
+
+(define (real-part-rectangular z) (car z))
+(define (imag-part-rectangular z) (cdr z))
+
+(define (magnitude-rectangular z)
+  (sqrt (+ (square (real-part-rectangular z))
+	   (square (imag-part-rectangular z)))))
+
+(define (angle-rectangular z)
+  (atan (imag-part-rectangular z)
+	(real-part-rectangular z)))
+
+(define (make-from-real-imag-rectangular x y)
+  (attach-tag 'rectangular (cons x y)))
+
+(define (make-from-mag-ang-rectangular r a)
+  (attach-tag
+   'rectangular
+   (cons (* r (cos a)) (* r (sin a)))))
+
+
+;; Polar Representation
+
+(define (real-part-polar z)
+  (* (magnitude-polar z)
+     (cos (angle-polar z))))
+
+(define (imag-part-polar z)
+  (* (magnitude-polar z)
+     (sin (angle-polar z))))
+
+(define (magnitude-polar z) (car z))
+(define (angle-polar z) (cdr z))
+
+(define (make-from-real-imag-polar x y)
+  (attach-tag
+   'polar
+   (cons (sqrt (+ (square x) (square y)))
+	 (atan y x))))
+
+(define (make-from-mag-ang-polar r a)
+  (attach-tag 'polar (cons r a)))
+
+
+;; Generic Selectors
+
 (define (real-part z)
-  (* (magnitude z) (cos (angle z))))
+  (cond ((rectangular? z)
+	 (real-part-rectangular (contents z)))
+	((polar? z)
+	 (real-part-polar (contents z)))
+	(else (error "Unknown type: REAL-PART" z))))
 
 (define (imag-part z)
-  (* (magnitude z) (sin (angle z))))
+  (cond ((rectangular? z)
+	 (imag-part-rectangular (contents z)))
+	((polar? z)
+	 (imag-part-polar (contents z)))
+	(else (error "Unknown type: IMAG-PART" z))))
 
-(define (magnitude z) (car z))
-(define (angle z) (cdr z))
+(define (magnitude z)
+  (cond ((rectangular? z)
+	 (magnitude-rectangular (contents z)))
+	((polar? z)
+	 (magnitude-polar (contents z)))
+	(else (error "Unknown type: MAGNITUDE" z))))
 
-(define (make-from-real-imag x y)
-  (cons (sqrt (+ (square x) (square y)))
-	(atan y x)))
+(define (angle z)
+  (cond ((rectangular? z)
+	 (angle-rectangular (contents z)))
+	((polar? z)
+	 (angle-polar (contents z)))
+	(else (error "Unknown type: ANGLE"))))
 
-(define (make-from-mag-ang r a)
-  (cons r a))
 
-;; operations
+;; Higher-Level Procedures
+
 (define (add-complex z1 z2)
   (make-from-real-imag
    (+ (real-part z1) (real-part z2))
-   (+ (imag-part z1) (real-part z2))))
+   (+ (imag-part z1) (imag-part z2))))
 
-(define (sub-complex z1 z2)
-  (make-form-real-imag
-   (- (real-part z1) (real-part z2))
-   (- (imag-part z1) (imag-part z2))))
+;; TODO: sub, mul, div
 
-(define (mul-complex z1 z2)
-  (make-from-real-imag
-   (* (magnitude z1) (magnitude z2))
-   (+ (angle z1) (angle z2))))
+(define (make-from-real-imag x y)
+  (make-from-real-imag-rectangular x y))
 
-(define (div-complex z1 z2)
-  (make-from-mag-ang
-   (/ (magnitude z1) (magnitude z2))
-   (- (angle z1) (angle z2))))
+(define (make-from-mag-ang r a)
+  (make-from-mag-ang-polar r a))
+
+
+;; Examples
+(define a (make-from-real-imag 3 4))
+(define b (make-from-mag-ang 3.5 52))
+(define c (add-complex a b))
+;; c
+;; (rectangular 2.4295322672150306 . 7.453196572141699)
+
