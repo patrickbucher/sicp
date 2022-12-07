0fb7fc54cd50c6e14b47b4ccae45e99244266f3d
Patrick Bucher <patrick.bucher@mailbox.org>
2022-12-07 07:32:07 +0100

    simplified examples; got it to run

--- examples/data-directed-complex-numbers.scm
+++ examples/data-directed-complex-numbers.scm
@@ -1,3 +1,6 @@
+(load "examples/op-type-table.scm")
+
+
 ;; Rectangular Implementation
 
 (define (install-rectangular-package)
@@ -16,10 +19,10 @@
   ;; interface to the rest of the system
   (define (tag x)
     (attach-tag 'rectangular x))
-  (put 'real-part '(rectangular) real-part)
-  (put 'imag-part '(rectangular) imag-part)
-  (put 'magnitude '(rectangular) magnitude)
-  (put 'angle '(rectangular) angle)
+  (put 'real-part 'rectangular real-part)
+  (put 'imag-part 'rectangular imag-part)
+  (put 'magnitude 'rectangular magnitude)
+  (put 'angle 'rectangular angle)
   (put 'make-from-real-imag 'rectangular
        (lambda (x y)
 	 (tag (make-from-real-imag x y))))
@@ -30,6 +33,7 @@
 
 
 ;; Polar Implementation
+
 (define (install-polar-package)
   ;; internal procedures
   (define (magnitude z) (car z))
@@ -45,10 +49,10 @@
 	  (atan y x)))
   ;; interface to the rest of the system
   (define (tag x) (attach-tag 'polar x))
-  (put 'real-part '(polar) real-part)
-  (put 'imag-part '(polar) imag-part)
-  (put 'magnitude '(polar) magnitude)
-  (put 'angle '(polar) angle)
+  (put 'real-part 'polar real-part)
+  (put 'imag-part 'polar imag-part)
+  (put 'magnitude 'polar magnitude)
+  (put 'angle 'polar angle)
   (put 'make-from-real-imag 'polar
        (lambda (x y)
 	 (tag (make-from-real-imag x y))))
@@ -57,14 +61,17 @@
 	 (tag (make-from-mag-ang r a))))
   'done)
 
-(define (apply-generic op . args)
-  (let ((type-tags  (map type-tag args)))
-    (let ((proc (get op type-tags)))
+
+;; generic procedures
+
+(define (apply-generic op arg)
+  (let ((tag (type-tag arg)))
+    (let ((proc (get op tag)))
       (if proc
-	  (apply proc (map contents args))
+	  (proc (contents arg))
 	  (error
 	   "No method for these types: APPLY-GENERIC"
-	   (list op type-tags))))))
+	   (list op tag))))))
 
 (define (real-part z)
   (apply-generic 'real-part z))
@@ -85,3 +92,41 @@
 	'polar)
    r a))
 
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+
+;; example usage
+
+(install-rectangular-package)
+(install-polar-package)
+
+(define z1 (make-from-real-imag 3 4))
+(define z2 (make-from-mag-ang 5 50))
+
+;; (real-part z1)
+;; 3
+;; (real-part z2)
+;; 4.824830142460566
+;; (imag-part z1)
+;; 4
+;; (imag-part z2)
+;; -1.3118742685196438
+;; (magnitude z1)
+;; 5
+;; (magnitude z2)
+;; 5
+;; (angle z1)
+;; .9272952180016122
+;; (angle z2)
+;; 50
