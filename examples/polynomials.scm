2d5b84b3f00e27069c230b7da08d40f227d487b6
Patrick Bucher <patrick.bucher@mailbox.org>
2023-01-05 12:29:50 +0100

    small re-work of polynomial example (still not working)

--- examples/polynomials.scm
+++ examples/polynomials.scm
@@ -1,32 +1,26 @@
-(load "examples/op-type-table.scm")
+(load "examples/op-type-table.scm") ;; set, get
+(load "examples/generic-arithmetic.scm") ;; add
 
 (define (install-polynomial-package)
   ;; internal procedures
   (define (add-terms L1 L2)
-    
-    ;; NOTE: teporary, drop-in solution
-    (define (add a b) (display (string "oh lawd " a)) (newline) (+ a b))
-    
     (cond ((empty-termlist? L1) L2)
 	  ((empty-termlist? L2) L1)
 	  (else
 	   (let ((t1 (first-term L1))
 		 (t2 (first-term L2)))
 	     (cond ((> (order t1) (order t2))
-		    (adjoin-term t1
-				 (add-terms (rest-terms L1)
-					    L2)))
+		    (adjoin-term
+		     t1 (add-terms (rest-terms L1) L2)))
 		   ((< (order t1) (order t2))
-		    (adjoin-term t2
-				 (add-terms L1
-					    (rest-terms L2))))
+		    (adjoin-term
+		     t2 (add-terms L1 (rest-terms L2))))
 		   (else
-		    (adjoin-term (make-term (order t1)
-					    (add (coeff t1)
-						 (coeff t2)))
-				 (add-terms
-				  (rest-terms L1)
-				  (rest-terms L2)))))))))
+		    (adjoin-term
+		     (make-term (order t1)
+				(add (coeff t1) (coeff t2)))
+		     (add-terms (rest-terms L1)
+				(rest-terms L2)))))))))
   (define (mul-terms L1 L2)
     (if (empty-termlist? L1)
 	(the-empty-termlist)
@@ -36,16 +30,17 @@
     (if (empty-termlist? L)
 	(the-empty-termlist)
 	(let ((t2 (first-term L)))
-	  (adjoin-term (make-term (+ (order t1) (order t2))
-				  (mul (coeff t1) (coeff t2)))
-		       (mul-term-by-all-terms t1 (rest-terms L))))))
+	  (adjoin-term
+	   (make-term (+ (order t1) (order t2))
+		      (mul (coeff t1) (coeff t2)))
+	   (mul-term-by-all-terms t1 (rest-terms L))))))
   ;; representation of poly
   (define (make-poly variable term-list)
     (cons variable term-list))
   (define (variable p) (car p))
   (define (term-list p) (cdr p))
   ;; representation of terms and term lists
-    (define (adjoin-term term term-list)
+  (define (adjoin-term term term-list)
     (if (=zero? (coeff term))
 	term-list
 	(cons term term-list)))
@@ -57,44 +52,41 @@
   (define (order term) (car term))
   (define (coeff term) (cadr term))
   (define (add-poly p1 p2)
-    (if (same-variable? (variable p1)
-			(variable p2))
+    (if (same-variable? (variable p1) (variable p2))
 	(make-poly (variable p1)
 		   (add-terms (term-list p1)
 			      (term-list p2)))
-	(error "Polys not in same var: ADD-POLY"
+	(error "Polys not in same var -- ADD-POLY"
 	       (list p1 p2))))
   (define (mul-poly p1 p2)
-    (if (same-variable? (variable p1)
-			(variable p2))
+    (if (same-variable? (variable p1) (variable p2))
 	(make-poly (variable p1)
 		   (mul-terms (term-list p1)
 			      (term-list p2)))
-	(error ("Polys not in same var: MUL-POLY"
+	(error ("Polys not in same var -- MUL-POLY"
 		(list p1 p2)))))
   ;; procedures from earlier examples
   (define (attach-tag tag object)
     (cons tag object))
   (define (same-variable? v1 v2)
-  (and (variable? v1)
-       (variable? v2)
-       (eq? v1 v2)))
+    (and (variable? v1)
+	 (variable? v2)
+	 (eq? v1 v2)))
   (define (variable? x) (symbol? x))
   ;; interface to the rest of the system
   (define (tag p) (attach-tag 'polynomial p))
   (put 'add '(polynomial polynomial)
-       (lambda (p1 p2)
-	 (tag (add-poly p1 p2))))
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
   (put 'mul '(polynomial polynomial)
-       (lambda (p1 p2)
-	 (tag (mul-poly p1 p2))))
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
   (put 'make 'polynomial
-       (lambda (var terms)
-	 (tag (make-poly var terms))))
+       (lambda (var terms) (tag (make-poly var terms))))
   'done)
 
 (install-polynomial-package)
 
-(define a ((get 'make 'polynomial) (list 'x 3) '(1 2 3)))
-(define b ((get 'make 'polynomial) (list 'x 3) '()))
+(define a ((get 'make 'polynomial) 'x '((3 2) (2 1) (1 4) (0 5))))
+(define b ((get 'make 'polynomial) 'x '((3 1) (2 0) (1 5) (0 3))))
+(define c ((get 'make 'polynomial) 'x '((3 3) (2 1) (1 9) (0 8))))
+(define d ((get 'make 'polynomial) 'x '()))
 ;; ((get 'add '(polynomial polynomial)) a b)
