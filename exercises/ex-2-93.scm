39feba80e76faa67b8edbfa16ee74d8df9a658f8
Patrick Bucher <patrick.bucher@mailbox.org>
2023-01-24 07:48:11 +0100

    exercise 2.93 [WIP]: add procedure

--- exercises/ex-2-93.scm
+++ exercises/ex-2-93.scm
@@ -1,10 +1,32 @@
 (load "exercises/ex-2-90.scm")
+(load "lib/functools.scm") ;; all-eq?
 
 (define (install-rational-package)
   ;; internal procedures
   (define (make-rational dividend divisor)
-    (tag (cons dividend divisor)))
-  ;; TODO add, sub, ...
+    (tag (list dividend divisor)))
+  (define (add-rf rf1 rf2)
+    (let ((dividend1 (car rf1))
+	  (dividend2 (car rf2))
+	  (divisor1 (cadr rf1))
+	  (divisor2 (cadr rf2)))
+      (let ((var-dividend1 (cadr dividend1))
+	    (var-dividend2 (cadr dividend2))
+	    (var-divisor1 (cadr divisor1))
+	    (var-divisor2 (cadr divisor2)))
+	(if (not (all-eq? (list var-dividend1 var-dividend2 var-divisor1 var-divisor2)))
+	    (error "cannot add rationals of different variables")
+	    (let ((tl-dividend1 (cddr dividend1))
+		  (tl-dividend2 (cddr dividend2))
+		  (tl-divisor1 (cddr divisor1))
+		  (tl-divisor2 (cddr divisor2)))
+	      (let ((new-divisor (apply-generic 'mul tl-divisor1 tl-divisor2)))
+		(let ((new-dividend
+		       (apply-generic 'add
+				      (apply-generic 'mul new-divisor tl-dividend1)
+				      (apply-generic 'mul new-divisor tl-dividend2))))
+		  ;; TODO: properly tag as polynomial
+		  (make-rational new-dividend new-divisor))))))))
   ;; type system
   (define (tag data)
     (cons 'ratfunc data))
@@ -25,3 +47,7 @@
 (define c2 ((get 'make 'counted) '((3 1) (0 1))))
 (define p1 ((get 'make 'polynomial) 'x c1))
 (define p2 ((get 'make 'polynomial) 'x c2))
+
+(define rf ((get 'make 'ratfunc) p1 p2))
+
+;; (apply-generic 'add rf rf)
