4f3eb4cb2317de0aeabead481a65365c82cad7d4
Patrick Bucher <patrick.bucher@mailbox.org>
2023-02-26 10:15:39 +0100

    exercise 3.39

---
+++ exercises/ex-3-39.rkt
@@ -0,0 +1,20 @@
+(load "lib/parallel-execute.rkt")
+(load "lib/serializer.rkt")
+
+(define x 10)
+
+(define s (make-serializer))
+
+(parallel-execute (lambda () (set! x ((s (lambda () (* x x))))))
+		  (s (lambda () (set! x (+ x 1)))))
+
+(display x)
+(newline)
+
+;; The computations won't be interrupted, but the order of the
+;; computations is still not deterministic.
+
+;; 1) P2 increments x, P1 squares it -> 121
+;; 2) P1 squares x, P2 increments it -> 101
+;; 3) P1 starts, P2 increments x, P1 overwrites it with the square -> 100
+
