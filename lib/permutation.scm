d773ddd4007e90b5b75216b50a6b3a043b5cb203
Patrick Bucher <patrick.bucher@mailbox.org>
2023-02-27 07:13:03 +0100

    permutations: before? predicate to filter outcomes by constraints

--- lib/permutation.scm
+++ lib/permutation.scm
@@ -28,3 +28,16 @@
 			     (map (lambda (p) (cons head p))
 				  perms))))
 		       headed)))))
+
+(define (before? permutations x y)
+  (define (next elements got-x?)
+    (cond ((null? elements)
+	   #f)
+	  ((equal? (car elements) x)
+	   (next (cdr elements) #t))
+	  ((equal? (car elements) y)
+	   got-x?)
+	  (else
+	   (next (cdr elements) got-x?))))
+  (filter (lambda (p) (next p #f)) permutations))
+
