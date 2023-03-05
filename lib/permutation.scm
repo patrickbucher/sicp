1aa8de60d03e8fd1a22f3c0c3159621be1f6ad95
Patrick Bucher <patrick.bucher@mailbox.org>
2023-03-05 11:34:36 +0100

    formatted code

--- lib/permutation.scm
+++ lib/permutation.scm
@@ -1,24 +1,24 @@
 (define (permutate xs)
   (define (next left right acc)
     (if (null? right)
-	acc
-	(next (append left (list (car right)))
-	      (cdr right)
-	      (append acc (map (lambda (p) (cons (car right) p))
-			       (permutate (append left (cdr right))))))))
+      acc
+      (next (append left (list (car right)))
+            (cdr right)
+            (append acc (map (lambda (p) (cons (car right) p))
+                             (permutate (append left (cdr right))))))))
   (cond ((null? xs) '())
-	((= (length xs) 1) (list xs))
-	(else (next '() xs '()))))
+        ((= (length xs) 1) (list xs))
+        (else (next '() xs '()))))
 
 (define (before? permutations x y)
   (define (next elements got-x?)
     (cond ((null? elements)
-	   #f)
-	  ((equal? (car elements) x)
-	   (next (cdr elements) #t))
-	  ((equal? (car elements) y)
-	   got-x?)
-	  (else
-	   (next (cdr elements) got-x?))))
+           #f)
+          ((equal? (car elements) x)
+           (next (cdr elements) #t))
+          ((equal? (car elements) y)
+           got-x?)
+          (else
+            (next (cdr elements) got-x?))))
   (filter (lambda (p) (next p #f)) permutations))
 
