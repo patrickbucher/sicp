fe32bf006e81a0bc3cc47eb4e49ba2bfd64bd796
Patrick Bucher <patrick.bucher@mailbox.org>
2023-03-06 07:24:48 +0100

    exercise 3.47 [WIP]: manage stacks using semaphores

--- exercises/ex-3-47.rkt
+++ exercises/ex-3-47.rkt
@@ -10,11 +10,32 @@
 (define (mutex-semaphore n)
   (if (< n 1)
       (error "cannot create semaphore of size" n)
-      (let ((mutexes (map (lambda (_) (make-mutex))
-			  (enumerate n))))
+      ;; TODO: mutexes to hash
+      (let ((mutexes ((foldl (lambda (x acc)
+			      (append acc (list x (make-mutex))))
+			    '()
+			    (enumerate n)))))
 	(define (dispatch message)
-	  (cond ((eq? message 'acquire)
-		 (let ((free (filter (lambda ;; TODO
+	  (cond ((eq? message 'acquire) false)
 		((eq? message 'relese) false)
 		(else (error "unknown message" message))))
 	dispatch)))
+
+(define (new-stack)
+  (let ((stack '()))
+    (define (push x)
+      (set! stack (cons x stack)))
+    (define (pop)
+      (if (null? stack)
+	  (error "stack is empty")
+	  (begin
+	    (let ((tip (car stack)))
+	      (set! stack (cdr stack))
+	      tip))))
+    (define (dispatch message)
+      (cond ((eq? message 'push) push)
+	    ((eq? message 'pop) pop)
+	    (else (error "unknown operation" message))))
+    dispatch))
+
+
