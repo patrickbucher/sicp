99fa6953745c5c2803f5792717e5050e3e51a7a4
Patrick Bucher <patrick.bucher@mailbox.org>
2023-07-14 09:00:33 +0200

    examples: infinite streams

---
+++ examples/infinite-streams.scm
@@ -0,0 +1,15 @@
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (divisible? x y) (= (remainder x y) 0))
+
+(define no-sevens
+  (stream-filter (lambda (x) (not (divisible? x 7)))
+		 integers))
+
+(define (fibgen a b)
+  (cons-stream a (fibgen b (+ a b))))
+
+(define fibs (fibgen 0 1))
